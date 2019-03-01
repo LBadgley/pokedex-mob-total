14ad2095d62b596375e058e74589959fa26266e5
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-01 14:21:49 -0800

    loadFilterOptions fn renders type choices, tests continue to pass

--- src/filter-component.js
+++ src/filter-component.js
@@ -7,3 +7,33 @@ export function createOption(type) {
     const dom = template.content;
     return dom;
 }
+
+export const types = [
+    'normal',
+    'fighting',
+    'flying',
+    'poison',
+    'ground',
+    'rock',
+    'bug',
+    'ghost',
+    'steel',
+    'fire',
+    'water',
+    'grass',
+    'electric',
+    'psychic',
+    'ice',
+    'dragon',
+    'dark',
+    'fairy'
+];
+
+const typeChoice = document.getElementById('type-choice');
+
+export default function loadFilterOptions(types) {
+    types.forEach(type => {
+        const dom = createOption(type);
+        typeChoice.appendChild(dom);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import pokedex from '../data/pokedex.js';
 import loadCards from './card-component.js';
+import loadFilterOptions, { types } from './filter-component.js';
 
+loadFilterOptions(types);
 loadCards(pokedex);
