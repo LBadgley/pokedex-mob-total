9137d4cf6b8893a12c7db931fb9907bfbff3e017
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-01 15:41:19 -0800

    Second test for filter passing

--- test/filter-array.test.js
+++ test/filter-array.test.js
@@ -4,7 +4,7 @@ QUnit.module('filter array');
 
 function filterArray(array, filterOptions) {
     return array.filter(item => {
-        const hasType = filterOptions.type === item.type;
+        const hasType = !filterOptions.type || filterOptions.type === item.type;
 
         return hasType;
     });
@@ -25,6 +25,31 @@ test('filter an array based on blue', assert => {
     const expected = [
         { type: 'blue', id: 1 },
         { type: 'blue', id: 2 }        
+    ];
+
+    // act
+    const result = filterArray(array, filterOptions);
+
+    // assert
+    assert.deepEqual(result, expected);
+});
+
+test('return all items if no selection', assert => {
+    // arrange
+    const array = [
+        { type: 'blue', id: 1 },
+        { type: 'green', id: 2 },
+        { type: 'blue', id: 2 }
+    ];
+    
+    const filterOptions = {
+        type: ''
+    };
+
+    const expected = [
+        { type: 'blue', id: 1 },
+        { type: 'green', id: 2 },
+        { type: 'blue', id: 2 }      
     ];
 
     // act
