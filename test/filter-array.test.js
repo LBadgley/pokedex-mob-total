fe90d0aa7739aafa205da501ed3bc141a3276037
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-01 15:39:33 -0800

    Passing first filter test

---
+++ test/filter-array.test.js
@@ -0,0 +1,35 @@
+const test = QUnit.test;
+
+QUnit.module('filter array');
+
+function filterArray(array, filterOptions) {
+    return array.filter(item => {
+        const hasType = filterOptions.type === item.type;
+
+        return hasType;
+    });
+}
+
+test('filter an array based on blue', assert => {
+    // arrange
+    const array = [
+        { type: 'blue', id: 1 },
+        { type: 'green', id: 2 },
+        { type: 'blue', id: 2 }
+    ];
+    
+    const filterOptions = {
+        type: 'blue'
+    };
+
+    const expected = [
+        { type: 'blue', id: 1 },
+        { type: 'blue', id: 2 }        
+    ];
+
+    // act
+    const result = filterArray(array, filterOptions);
+
+    // assert
+    assert.deepEqual(result, expected);
+});
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,4 @@
 import './html-equal.js';
 import './card-component.test.js';
 import './filter-component.test.js';
+import './filter-array.test.js';
